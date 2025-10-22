cc5944ba34fb34732ed3b1846c4c956467f87226
Funnypeace <funnypeace89@googlemail.com>
2025-10-22 15:41:30 +0200

    Fix import paths: correct services path and remove double slash in types

--- components/DamageReportWizard.tsx
+++ components/DamageReportWizard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { DamageReport, ReportStatus } from '../types';
-import { getReportById, createReport, updateReport } from './services/damageReportService';
+import { getReportById, createReport, updateReport } from '../services/damageReportService';
 import { toISOFromInput } from './src/lib/date';
 
 import ProgressBar from './ui/ProgressBar';
